74ed1d5f46e054daa639636f2c08f2cb728e2b8c
Naveentech1999 <121205704+Naveentech1999@users.noreply.github.com>
2023-08-30 12:35:05 +0530

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,14 +1,5 @@
-@echo off
-
-rem Set the git user name and email
-git config user.name "Naveentech1999"
-git config user.email "naveenbhupathi22@gmail,com"
-
-rem Define the ORIGIN variable
-set ORIGIN="https://github.com/Naveentech1999/sample.git"
-
-rem Delete the tag
-git tag -d %DEV%
-
-rem Push the changes to the remote repository
-git push %ORIGIN%
+for t in `git tag`
+do
+    git push origin :$t
+    git tag -d $t
+done
