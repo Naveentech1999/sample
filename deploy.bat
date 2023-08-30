8357c9fc29f359dda6b3558191cb333fe7443723
Naveentech1999 <121205704+Naveentech1999@users.noreply.github.com>
2023-08-30 12:30:27 +0530

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,21 +1,14 @@
 @echo off
 
- 
+rem Set the git user name and email
+git config user.name "Naveentech1999"
+git config user.email "naveenbhupathi22@gmail,com"
+
 rem Define the ORIGIN variable
 set ORIGIN="https://github.com/Naveentech1999/sample.git"
-echo %ORIGIN%
 
-rem Temporarily disable the error-checking flag
-set +e
+rem Delete the tag
+git tag -d %DEV%
 
-rem Delete the tag if it already exists
-git tag -d %TAG%
-
-rem Push the tag to the remote repository
-git push %ORIGIN% :refs/tags/%TAG%
-
-rem Re-enable the error-checking flag
-set -e
-
-rem Print a message to confirm that the tag was added
-echo "TAG %TAG% added to %ORIGIN%"
+rem Push the changes to the remote repository
+git push %ORIGIN%
