5ff6c4a1b96c1566420091255b2ff723b7807d1f
Naveentech1999 <121205704+Naveentech1999@users.noreply.github.com>
2023-08-30 15:46:51 +0530

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,18 @@
 @echo off
+setlocal enabledelayedexpansion
 
-rem Set the git user name and email
-git config user.name %GIT_USERNAME%
-git config user.email %GIT_USERNAME%
+rem Get arguments
+set "STAGE=%1"
+set "REGION=%2"
+set "TAG=%3"
+set "GIT_USERNAME=%4"
+set "GIT_PASSWORD=%5"
+set "BUILD_NUMBER=%6"
+set "BUILD_URL=%7"
+set "RELEASE_VERSION=%8"
 
-rem Define the ORIGIN variable
-set ORIGIN="https://%GIT_USERNAME%:%GIT_PASSWORD%@https://github.com/Naveentech1999/sample.git"
+rem Define ORIGIN variable
+set "ORIGIN=https://github.com/Naveentech1999/sample.git
 echo %ORIGIN%
 
 rem Temporarily disable the error-checking flag
@@ -20,5 +27,10 @@ git push %ORIGIN% :refs/tags/%TAG%
 rem Re-enable the error-checking flag
 set -e
 
-rem Print a message to confirm that the tag was added
-echo "TAG %TAG% added to %ORIGIN%"
+rem Create and push the new tag
+git tag -a %TAG% -m "Tagged by Jenkins build no.%BUILD_NUMBER% on %date%, STAGE: %STAGE%, REGION: %REGION%, URL: %BUILD_URL%"
+git push %ORIGIN% --tags
+
+echo TAG %TAG% added to %ORIGIN%
+
+endlocal
