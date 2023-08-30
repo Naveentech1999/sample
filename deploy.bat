57e0032f27ae8b1d349a9a5a2277b38e725e1929
Naveentech1999 <121205704+Naveentech1999@users.noreply.github.com>
2023-08-30 16:48:06 +0530

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,42 +1,10 @@
 @echo off
-setlocal enabledelayedexpansion
 
-rem Get arguments
-set "STAGE=%1"
-set "REGION=%2"
-set "TAG=%3"
-set "GIT_USERNAME=%4"
-set "GIT_PASSWORD=%5"
-set "BUILD_NUMBER=%6"
-set "BUILD_URL=%7"
-set "RELEASE_VERSION=%8"
+rem Get the tag name from the environment variable
+set TAG=%1
 
-rem Define ORIGIN variable
-set "ORIGIN=https://%GIT_USERNAME%:%GIT_PASSWORD%@https://github.com/Naveentech1999/sample.git"
-echo ORIGIN: %ORIGIN%
-
-rem Display the PATH variable for debugging
-echo PATH: %PATH%
-
-rem Display the location of the git executable for debugging
-where git
-
-rem Temporarily disable the error-checking flag
-set +e
-
-rem Delete the tag if it already exists
+rem Delete the tag locally
 git tag -d %TAG%
 
-rem Push the tag to the remote repository
-git push %ORIGIN% :refs/tags/%TAG%
-
-rem Re-enable the error-checking flag
-set -e
-
-rem Create and push the new tag
-git tag -a %TAG% -m "Tagged by Jenkins build no.%BUILD_NUMBER% on %date%, STAGE: %STAGE%, REGION: %REGION%, URL: %BUILD_URL%"
-git push %ORIGIN% --tags
-
-echo TAG %TAG% added to %ORIGIN%
-
-endlocal
+rem Delete the tag remotely
+git push origin :refs/tags/%TAG%
