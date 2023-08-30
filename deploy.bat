fad02ad92898e39222eeafbb8da328d56620b57b
Naveentech1999 <121205704+Naveentech1999@users.noreply.github.com>
2023-08-30 12:04:48 +0530

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,24 +1,26 @@
 @echo off
+setlocal
 
-rem Set the git user name and email
-git config user.name %GIT_USERNAME%
-git config user.email %GIT_USERNAME%
+rem Define variables
+set TAG=DEV
+set ORIGIN=https://github.com/Naveentech1999/sample.git
 
-rem Define the ORIGIN variable
-set ORIGIN="https://%GIT_USERNAME%:%GIT_PASSWORD%@https://github.com/Naveentech1999/sample.git"
 echo %ORIGIN%
 
-rem Temporarily disable the error-checking flag
-set +e
+rem Disable breaking on errors
+setlocal enabledelayedexpansion
+set "errorlevel="
+goto :noError
 
-rem Delete the tag if it already exists
-git tag -d %TAG%
+:noError
+  rem Delete the Git tag locally
+  git tag -d %TAG%
+  rem Push the deletion of the Git tag to the remote repository
+  git push %ORIGIN% :refs/tags/%TAG%
 
-rem Push the tag to the remote repository
-git push %ORIGIN% :refs/tags/%TAG%
+rem Re-enable breaking on errors
+endlocal
 
-rem Re-enable the error-checking flag
-set -e
+echo TAG %TAG% deleted from %ORIGIN%
 
-rem Print a message to confirm that the tag was added
-echo "TAG %TAG% added to %ORIGIN%"
+endlocal
